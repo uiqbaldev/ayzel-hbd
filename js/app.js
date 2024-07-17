f376e1f402ef1c821a1a667f15bf2b9de6c8eb85
uiqbaldev <118964822+uiqbaldev@users.noreply.github.com>
2024-07-17 19:27:27 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const topElement = document.querySelector("#home");
+
+    if (topElement) {
+        topElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+});
+const util = (() => {
+
+    const opacity = (id) => {
+        let nm = document.getElementById(id);
+        let op = parseInt(nm.style.opacity);
+        let clear = null;
+
+        clear = setInterval(() => {
+            if (op >= 0) {
+                nm.style.opacity = op.toString();
+                op -= 0.025;
+            } else {
+                clearInterval(clear);
+                clear = null;
+                nm.remove();
+                return;
+            }
+        }, 10);
+    };
+
+    const escapeHtml = (unsafe) => {
+        return unsafe
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
+    };
+
+    const timer = () => {
+        let countDownDate = (new Date(document.getElementById('date').getAttribute('data-date').replace(' ', 'T'))).getTime();
+
+        setInterval(() => {
+            let distance = Math.abs(countDownDate - (new Date()).getTime());
+
+            document.getElementById('day').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+            document.getElementById('hour').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            document.getElementById('minute').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            document.getElementById('seconds').innerText = Math.floor((distance % (1000 * 60)) / 1000);
+        }, 1000);
+    };
+
+    const play = (btn) => {
+        if (btn.getAttribute('data-status') !== 'true') {
+            btn.setAttribute('data-status', 'true');
+            audio.play();
+            btn.innerHTML = '<i class="fa-solid fa-circle-pause"></i>';
+        } else {
+            btn.setAttribute('data-status', 'false');
+            audio.pause();
+            btn.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
+        }
+    };
+	
+    const animation = async () => {
+        const duration = 10 * 1000;
+        const animationEnd = Date.now() + duration;
+        let skew = 1;
+
+        let randomInRange = (min, max) => {
+            return Math.random() * (max - min) + min;
+        };
+
+        (async function frame() {
+            const timeLeft = animationEnd - Date.now();
+            const ticks = Math.max(200, 500 * (timeLeft / duration));
+
+            skew = Math.max(0.8, skew - 0.001);
+
+            await confetti({
+                particleCount: 1,
+                startVelocity: 0,
+                ticks: ticks,
+                origin: {
+                    x: Math.random(),
+                    y: Math.random() * skew - 0.2,
+                },
+                colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+                shapes: ["hearts"],
+                gravity: randomInRange(0.5, 1),
+                scalar: randomInRange(1, 2),
+                drift: randomInRange(-0.5, 0.5),
+            });
+
+            if (timeLeft > 0) {
+                requestAnimationFrame(frame);
+            }
+        })();
+    };
+
+    const openinvite = async (button) => {
+        button.disabled = true;
+        document.querySelector('body').style.overflowY = 'scroll';
+        AOS.init();
+        audio.play();
+		
+        opacity('welcome');
+        document.getElementById('music').style.display = 'block';
+        timer();
+
+        await confetti({
+            origin: { y: 0.8 },
+            zIndex: 1057
+        });
+        await animation();
+    };
+	
+	const download = (url) => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = url;
+        downloadLink.download = 'card';
+        downloadLink.click();
+    };
+
+    return {
+        openinvite,
+        play,
+        escapeHtml,
+        opacity,
+		download,
+    };
+})();
+
+const audio = (() => {
+    let audio = null;
+
+    const singleton = () => {
+        if (!audio) {
+            audio = new Audio();
+            audio.src = document.getElementById('music').getAttribute('data-url');
+            audio.load();
+            audio.currentTime = 0;
+            audio.autoplay = true;
+            audio.muted = false;
+            audio.loop = true;
+            audio.volume = 1;
+        }
+
+        return audio;
+    };
+
+    return {
+        play: () => singleton().play(),
+        pause: () => singleton().pause(),
+    };
+})();
